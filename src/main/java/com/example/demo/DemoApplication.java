6c6b51a94c4419f303b830e9390c2099b8268046
Shihyu Ho <methodho@gmail.com>
2025-07-25 09:23:26 +0800

    feat: use jasypt api to detector encrypt property

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyDetector;
 import com.ulisesbocchio.jasyptspringboot.EncryptablePropertySourceConverter;
-import com.ulisesbocchio.jasyptspringboot.properties.JasyptEncryptorConfigurationProperties;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
@@ -11,53 +11,52 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertySource;
 
-import java.util.function.Predicate;
-
-import static com.example.demo.DemoApplication.EncryptValuePropertySource.ENCRYPT_PROPERTY_SOURCE_NAME;
-import static com.ulisesbocchio.jasyptspringboot.properties.JasyptEncryptorConfigurationProperties.bindConfigProps;
-
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
-	@Bean
-	public static BeanFactoryPostProcessor encryptValuePropertySourcePostProcessor(ConfigurableEnvironment environment,
-			EncryptablePropertySourceConverter converter) {
-		return new EncryptValuePropertySourcePostProcessor(environment, converter);
-	}
+  @Bean
+  public static BeanFactoryPostProcessor encryptValuePropertySourcePostProcessor(
+      ConfigurableEnvironment environment,
+      EncryptablePropertySourceConverter converter,
+      EncryptablePropertyDetector detector) {
+    return new EncryptValuePropertySourcePostProcessor(environment, converter, detector);
+  }
 
-	public record EncryptValuePropertySourcePostProcessor(ConfigurableEnvironment environment,
-			EncryptablePropertySourceConverter converter) implements BeanFactoryPostProcessor {
+  public record EncryptValuePropertySourcePostProcessor(
+      ConfigurableEnvironment environment,
+      EncryptablePropertySourceConverter converter,
+      EncryptablePropertyDetector detector)
+      implements BeanFactoryPostProcessor {
 
-		@Override
-		public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-			var properties = bindConfigProps(environment);
-			environment.getPropertySources()
-				.addLast(converter.makeEncryptable(new EncryptValuePropertySource(ENCRYPT_PROPERTY_SOURCE_NAME,
-						properties.getProperty().getPrefix(), properties.getProperty().getSuffix())));
-		}
-	}
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
+        throws BeansException {
+      environment
+          .getPropertySources()
+          .addLast(converter.makeEncryptable(new EncryptValuePropertySource(detector)));
+    }
+  }
 
-	public static class EncryptValuePropertySource extends PropertySource<Predicate<String>> {
+  public static class EncryptValuePropertySource
+      extends PropertySource<EncryptablePropertyDetector> {
 
-		public static final String ENCRYPT_PROPERTY_SOURCE_NAME = "encrypt";
+    public static final String ENCRYPT_PROPERTY_SOURCE_NAME = "encrypt";
 
-		public EncryptValuePropertySource(String name, String prefix, String suffix) {
-			this(name, propertyName -> propertyName.startsWith(prefix) && propertyName.endsWith(suffix));
-		}
+    public EncryptValuePropertySource(EncryptablePropertyDetector detector) {
+      this(ENCRYPT_PROPERTY_SOURCE_NAME, detector);
+    }
 
-		public EncryptValuePropertySource(String name, Predicate<String> matcher) {
-			super(name, matcher);
-		}
+    public EncryptValuePropertySource(String name, EncryptablePropertyDetector detector) {
+      super(name, detector);
+    }
 
-		@Override
-		public Object getProperty(String name) {
-			return getSource().test(name) ? name : null;
-		}
-
-	}
-
+    @Override
+    public Object getProperty(String name) {
+      return getSource().isEncrypted(name) ? name : null;
+    }
+  }
 }
